58bc9b4aa4057fc05b49a00895f386fe2e566026
remii <remi.gravelaine-isc_france@fr.ibm.com>
2017-02-28 18:17:54 +0100

    Ajout de commentaires

--- ProjetDeveloppement/BLL/AbonneManager.cs
+++ ProjetDeveloppement/BLL/AbonneManager.cs
@@ -10,6 +10,12 @@ namespace BLL
 {
     public class AbonneManager
     {
+
+        /// <summary>
+        /// Méthode de récuperation des informations de l'abonné
+        /// </summary>
+        /// <param name="name">Nom de l'abonné</param>
+        /// <returns>Retourne toutes les informations de l'abonné</returns>
         public AbonneEntity GetInfoAbonne(string name)
         {
             return new AbonneProvider().GetInfoAbonne(name);
